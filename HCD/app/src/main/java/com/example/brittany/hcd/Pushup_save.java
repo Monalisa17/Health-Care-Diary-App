8acde59fd4b453a91acbc25ae93b7875163049d5
darkensoul <wcollado@mail.usf.edu>
2015-10-27 11:17:00 -0400

    Push Up Parse
    - Pushup_save: adds to parse db under PushupCount succesfully, will say
    if failed.

--- HCD/app/src/main/java/com/example/brittany/hcd/Pushup_save.java
+++ HCD/app/src/main/java/com/example/brittany/hcd/Pushup_save.java
@@ -3,17 +3,29 @@ package com.example.brittany.hcd;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
 public class Pushup_save extends AppCompatActivity {
+
+    TextView pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pushup_save);
+
+        pass = (TextView) findViewById(R.id.textView_dbentry);
+
+
 
         // Get the parameters passed
         Bundle extras = getIntent().getExtras();
@@ -24,8 +36,32 @@ public class Pushup_save extends AppCompatActivity {
         TextView displayGoal = (TextView) findViewById(R.id.textView_goalSave);
         displayGoal.setText(pushdone_string);
 
-        if(Integer.parseInt(pushdone_string) > pushgoal_integer)
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "9lsXvAhxazTezFl8oTEhCnGr3p9S0qNetMNgmmgR", "ZYWsX8HmLCoEBFkfwZuPljn2VNiaqDomcMbkFIrk");
+
+        ParseObject PushupCount = new ParseObject("PushupCount");
+        PushupCount.put("Count", pushdone_string);
+        PushupCount.saveInBackground();
+
+        // Check if it saved, will display on the app
+        PushupCount.saveInBackground(new SaveCallback()
         {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    // Log.e("PARSE.COM", "FAILED" + e.getMessage());
+                    pass.setText("Did not save! ");
+                } else {
+                    // Log.e("PARSE.COM", "SUCCESS");
+                    pass.setText("Has been saved! ");
+
+                }
+            }
+        });
+
+
+        if(Integer.parseInt(pushdone_string) > pushgoal_integer) {
             Toast t = Toast.makeText(this, "CONGRATULATIONS YOU BEAT YOUR GOAL!!!", Toast.LENGTH_SHORT);
             t.show();
         }
@@ -66,7 +102,13 @@ public class Pushup_save extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        startActivity(new Intent(this, Workout_Main.class));
-        finish();
+
+        Intent intent = new Intent(Pushup_save.this, Workout_Main.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+        // startActivity(new Intent(this, Workout_Main.class));
+        // finish();
+
     }
 }
