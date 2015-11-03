ff213b4b94c3c279e3633f143637881ac0bab920
btuazon <btuazon@mail.usf.edu>
2015-11-03 13:22:23 -0500

    Save details activity change
    Added the save functionality to food save details activity

--- HCD/app/src/main/java/com/example/brittany/hcd/Food_Save_details.java
+++ HCD/app/src/main/java/com/example/brittany/hcd/Food_Save_details.java
@@ -1,16 +1,91 @@
 package com.example.brittany.hcd;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 
 public class Food_Save_details extends AppCompatActivity {
+
+    ImageView PhotoCapturedImageView;
+    ParseFile photoFile;
+    Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food__save_details);
+
+        bmp = null;
+        String filename = getIntent().getStringExtra("image");
+        try {
+            FileInputStream is = this.openFileInput(filename);
+            bmp = BitmapFactory.decodeStream(is);
+/*
+            //For parsefile
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            byte[] scaledData = bos.toByteArray();
+
+            photoFile = new ParseFile("meal_photo.jpg", scaledData);
+            photoFile.saveInBackground();
+*/
+
+            PhotoCapturedImageView = (ImageView)findViewById(R.id.foodView);
+            PhotoCapturedImageView.setImageBitmap(bmp);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void saveFoodEntry(View view){
+
+        EditText name = (EditText)findViewById(R.id.nameText);
+        EditText description = (EditText)findViewById(R.id.dText);
+
+        if(name != null && description != null){
+            // Create a New Class called "ImageUpload" in Parse
+            ParseObject imgupload = new ParseObject("FoodUpload");
+
+            // Create a column named "ImageName" and set the string
+            //imgupload.put("ImageName", name.getText().toString());
+            imgupload.put("ImageDescription", description.getText().toString());
+
+            //For parsefile
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            byte[] scaledData = bos.toByteArray();
+
+            //insert parse function here
+
+            photoFile = new ParseFile(name.getText().toString()+".jpg", scaledData);
+            photoFile.saveInBackground();
+
+            // Create a column named "ImageFile" and insert the image
+            imgupload.put("ImageFile", photoFile);
+
+            // Create the class and the columns
+            imgupload.saveInBackground();
+
+            // Show a simple toast message
+            Toast.makeText(Food_Save_details.this, "Image Uploaded",
+                    Toast.LENGTH_SHORT).show();}
+
     }
 
     @Override
